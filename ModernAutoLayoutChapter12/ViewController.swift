de6968a1a7ae3a5f2be151d034f4dabff4e2e426
Pavlomat <pavlov_matthew@mail.ru>
2022-11-11 11:06:49 +0300

    Initial Commit

---
+++ ModernAutoLayoutChapter12/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ModernAutoLayoutChapter12
+//
+//  Created by Pavlov Matvey on 11.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
